bc01572aeb4aed08e8683d36e365f14e9f6d73cb
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-27 17:19:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,14 +14,16 @@
 
 
 def speak_to_grandma(string)
-  if string == string.upcase
-    if string == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
-    else
-      puts "NO, NOT SINCE 1938!"
-    end
-  else
-    puts "HUH?! SPEAK UP, SONNY!"
-  end
   
+  if string == string.upcase
+    
+    if string == "I LOVE YOU GRANDMA!"
+      "I LOVE YOU TOO PUMPKIN!"
+    else
+      "NO, NOT SINCE 1938!"
+    end
+    
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
 end
